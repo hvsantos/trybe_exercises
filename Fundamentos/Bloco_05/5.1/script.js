dd56c6e13107e625ae7db27da94b9b7d5b91d1c0
Henrique Vilela <hv_santos@outlook.com>
2022-08-28 04:50:47 -0300

    Modify index.html and script.js

---
+++ Fundamentos/Bloco_05/5.1/script.js
@@ -0,0 +1,14 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const main_title = document.getElementById("page-title");
+main_title.innerHTML = "Esquadrão Classe A";
+
+const sub_paragraph = document.getElementById("second-paragraph");
+sub_paragraph.innerHTML = "Criatividade goes BRRRRRR";
+
+
+function sumNum (num1, num2) {
+    return num1 + num2;
+}
+console.log(sumNum(1, 2))
