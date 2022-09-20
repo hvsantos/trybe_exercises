6d3f381dfcc3bc6dc0ce50415d7e2376f3375272
Henrique Vilela <hv_santos@outlook.com>
2022-09-20 09:12:49 -0300

    Exec_06 done

---
+++ Fundamentos/Sec_07/7.3/exec_06/hydrate.js
@@ -0,0 +1,16 @@
+function hydrate(drinks) {
+  let sum = 0;
+  let result  = '';
+  const arrDrinks = drinks.split(' ')
+  for (let i = 0; i < arrDrinks.length; i += 1){
+    let num = Number(arrDrinks[i]);
+    if (!isNaN(num)){
+      sum += num;
+    }
+  }
+  (sum === 1) ? result = sum + ' copo de água' : result = sum + ' copos de água';
+  return result
+}
+
+
+module.exports = hydrate
