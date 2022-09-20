43c8f695aa667b130063469af284d500c2a042e9
Henrique Vilela <hv_santos@outlook.com>
2022-09-20 07:43:20 -0300

    Misc changes to the test description

--- Fundamentos/Sec_07/7.3/exec_01/sum.test.js
+++ Fundamentos/Sec_07/7.3/exec_01/sum.test.js
@@ -1,16 +1,16 @@
 const sum = require('./sum');
 
-describe('Teste função sum', () => {
-  it('Deve retornar 9 se sum(4,5)', () => {
+describe('Teste da função sum()', () => {
+  it('Deve retornar 9 se serem inseridos 4 e 5', () => {
     expect(sum(4,5)).toBe(9);
   });
-  it('Deve retornar 0 se sum(0,0)', () => {
+  it('Deve retornar 0 se serem inseridos 0 e 0', () => {
     expect(sum(0,0)).toBe(0);
   });
-  it(`Deve retornar um erro se sum(4,'5')`, () => {
+  it(`Deve retornar um erro se forem inseridos 4 e '5'`, () => {
     expect(() => sum(4,'5')).toThrow()
   });
-  it(`Deve retornar 'parameters must be numbers' se sum(4, '5')`, () => {
+  it(`Deve retornar 'parameters must be numbers' se serem inseridos 4 e '5'`, () => {
     expect(() => sum(4,'5')).toThrow(new Error('parameters must be numbers'))
   })
 })
--- Fundamentos/Sec_07/7.3/exec_02/myRemove.test.js
+++ Fundamentos/Sec_07/7.3/exec_02/myRemove.test.js
@@ -1,13 +1,13 @@
 const myRemove = require('./myRemove');
 
-describe('Teste da função myRemove', () => {
-  it('Retorne o array [1,2,4] se myRemove([1,2,3,4], 3)', () => {
+describe('Teste da função myRemove()', () => {
+  it('Retorne o array [1,2,4] se inserido como primeiro parametro [1,2,3,4] e o segundo parametro 3', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
   });
-  it('Não retorne o array [1,2,3,4] se myRemove([1,2,3,4], 3)', () => {
+  it('Não retorne o array [1,2,3,4] se o primeiro parametro [1,2,3,4] e o segundo 3', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
   });
-  it('Retorne o array [1,2,3,4] se myRemove([1,2,3,4], 5)', () => {
+  it('Retorne o array [1,2,3,4] se o primeiro parametro for [1,2,3,4] e o segundo 5', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
   });
 })
