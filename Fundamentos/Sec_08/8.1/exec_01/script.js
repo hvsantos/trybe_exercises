df6ef00ce3804fe08fbfeda717c4771aa7783d8e
Henrique Vilela <hv_santos@outlook.com>
2022-09-23 00:02:59 -0300

    exec_01 done

--- Fundamentos/Sec_08/8.1/exec_01/script.js
+++ Fundamentos/Sec_08/8.1/exec_01/script.js
@@ -1,16 +1,15 @@
 const genNameEmail = (cName) => {
-  const genEmail = `${cName.toLowerCase().replace(' ', '_')}@trybe.com`
-  return { name: cName, email: genEmail }
+  return { name: cName, email: `${cName.toLowerCase().replace(' ', '_')}@trybe.com` }
 }
 
-const newEmployees = (names) => {
+const newEmployees = (names, callFunc) => {
   const employees = {}
   for (let i = 0; i < names.length; i += 1){
-    employees[`id${i+1}`] = genNameEmail(names[i])
+    employees[`id${i+1}`] = callFunc(names[i])
   }
   return employees;
 };
 
 const arrNames = ['Pedro Guerra', 'Luiza Drumond', 'Carla Paiva']
 
-console.log(newEmployees(arrNames));
+console.log(newEmployees(arrNames, genNameEmail));
