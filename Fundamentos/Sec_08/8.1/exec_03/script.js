385f9defa9f7585c7b7b2e9c308b9c4b58a6cc00
Henrique Vilela <hv_santos@outlook.com>
2022-09-23 00:03:21 -0300

    exec_03 done

--- Fundamentos/Sec_08/8.1/exec_03/script.js
+++ Fundamentos/Sec_08/8.1/exec_03/script.js
@@ -1,14 +1,18 @@
 const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const studentAnswers = ['A', 'N.A', 'N.A', 'N.A', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const compareA = (checkRight, checkAnswer) => (checkRight === checkAnswer)? 1:0;
-
-const checkAnswers = (rAnswers, sAnswers, funcComp) => {
+const compareA = (checkRight, checkAnswer) => {
   let cont = 0;
-  for(let i = 0; i < rAnswers.length; i += 1) {
-    cont += funcComp(rAnswers[i], sAnswers[i])
-  }
-  return `Pontuação: ${cont}`
-}
+  for(let i = 0; i < checkRight.length; i += 1) {
+    if (checkAnswer[i] === checkRight[i]) {
+      cont += 1;
+    } else if (checkAnswer[i] !== 'N.A') {
+      cont -= 0.5;
+    }
+  };
+  return cont
+};
+
+const checkAnswers = (rAnswers, sAnswers, funcComp) => 'Pontuação: ' + funcComp(rAnswers, sAnswers);
 
 console.log(checkAnswers(rightAnswers, studentAnswers, compareA))
