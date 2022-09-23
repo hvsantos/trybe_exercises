87ab54502eef6964d75bddf25fd33e150f38672c
Henrique Vilela <hv_santos@outlook.com>
2022-09-23 00:03:14 -0300

    exec_02 done

--- Fundamentos/Sec_08/8.1/exec_02/script.js
+++ Fundamentos/Sec_08/8.1/exec_02/script.js
@@ -1,3 +1,3 @@
-const genNumber = () => Math.floor(Math.random() * 5) + 1
-
-const compareNumber = (number) => number === genNumber() ? 'Parabéns você ganhou' : 'Tente novamente';
+const genNumber = () => Math.floor(Math.random() * 5) + 1;
+const compareNumber = (number, funcName) => number === funcName() ? 'Parabéns você ganhou' : 'Tente novamente';
+console.log(compareNumber(3, genNumber));
