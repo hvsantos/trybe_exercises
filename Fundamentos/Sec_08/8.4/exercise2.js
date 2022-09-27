16766ce89e2363fff617315a249ba90aa2076a3a
Henrique Vilela <hv_santos@outlook.com>
2022-09-27 19:56:06 -0300

    8.4 exercise 2 done

---
+++ Fundamentos/Sec_08/8.4/exercise2.js
@@ -0,0 +1,79 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+const reduceNames = (array) => array.reduce((a, b, index) => {
+  let retorno = `${a}${b.author.name}`;
+  retorno += ((index === 5)? '.' : ', ');
+  return retorno;
+}, '')
+
+const averageAge = (array) => array.reduce((a, {releaseYear: bRY,author: {birthYear: bBY}}) => a+(bRY - bBY), 0) / array.length
+
+const longestNamedBook = (array) => array.reduce((a, b, index) => {
+  if (index === 0) { a = b }
+  if (a.name.length < b.name.length) { a = b }
+  return a
+}, '')
+
+console.log(longestNamedBook(books));
